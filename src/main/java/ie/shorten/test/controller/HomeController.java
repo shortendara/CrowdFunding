c2e9801152ee8325a725c78fda2a8b096bd2bb88
Dara Shorten <dara.shorten@gmail.com>
2016-11-22 14:36:39 +0000

    Removed unecessary imports

--- src/main/java/ie/shorten/test/controller/HomeController.java
+++ src/main/java/ie/shorten/test/controller/HomeController.java
@@ -1,20 +1,12 @@
 package ie.shorten.test.controller;
 
-import java.io.IOException;
+
 import java.util.List;
 import java.util.Locale;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
 import ie.shorten.test.entity.Product;
 import ie.shorten.test.repository.ProductRepository;
