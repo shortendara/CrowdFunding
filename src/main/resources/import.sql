b4212547915bfda5055c45cae7be3f5805d6ebf3
Dara Shorten <dara.shorten@gmail.com>
2016-11-22 12:11:37 +0000

    Changed from artist-movements to user_products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,4 @@ INSERT INTO users VALUES (1,'Chadwick, Lynn'),(2,'Ono, Yoko'),(3,'Opie, Julian')
 
 INSERT INTO products VALUES (1,'Geometry of Fear'),(2,'Kinetic Art'),(3,'New British Sculpture'),(4,'Romanticism');
 
-INSERT INTO artist_movements VALUES (1,1),(1,2),(3,3),(4,4),(5,4);
+INSERT INTO users_products VALUES (1,1),(1,2),(3,3),(4,4),(5,4);
