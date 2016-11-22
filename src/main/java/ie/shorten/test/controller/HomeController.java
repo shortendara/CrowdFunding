8403154b010603e1cc06e9925a5a534f910a3873
Dara Shorten <dara.shorten@gmail.com>
2016-11-22 14:35:38 +0000

    Created route for index page

--- src/main/java/ie/shorten/test/controller/HomeController.java
+++ src/main/java/ie/shorten/test/controller/HomeController.java
@@ -1,11 +1,20 @@
 package ie.shorten.test.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import ie.shorten.test.entity.Product;
 import ie.shorten.test.repository.ProductRepository;
@@ -13,23 +22,19 @@ import ie.shorten.test.repository.ProductRepository;
 @Controller
 public class HomeController {
 	
+	@Autowired
 	ProductRepository product_repository;
 	
 	@RequestMapping("/")
-	public String index(Locale locale, Model model){
-		List<Product> product_list = product_repository.findAll();
-		model.addAttribute(product_list);
+	public String index(){
 		return "index";
 	}
 	
-	@RequestMapping("/user")
-	public String user(Model model){
-		return null;
-	}
-	
-	@RequestMapping("/user/products")
-	public String user_products(Model model){
-		return null;
+	@RequestMapping("/allProducts")
+	public String index(Model model){
+		List<Product> product_list = product_repository.findAll();
+		model.addAttribute(product_list);
+		return "allProducts";
 	}
 	
 }
