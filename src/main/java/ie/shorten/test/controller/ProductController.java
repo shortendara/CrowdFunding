01652ad45ab903e8ecf02c678826b7e1e4127ed7
darrensisk <darrensisk1995@yahoo.ie>
2016-11-22 16:01:51 +0000

    Request mapping of product.html moved to product controller

--- src/main/java/ie/shorten/test/controller/ProductController.java
+++ src/main/java/ie/shorten/test/controller/ProductController.java
@@ -1,8 +1,28 @@
 package ie.shorten.test.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ie.shorten.test.entity.Product;
+import ie.shorten.test.repository.ProductRepository;
 
 @Controller
 public class ProductController {
+	
+	@Autowired
+	ProductRepository product_repository;
+	
+	
+	@RequestMapping("/product/{id}")
+	public String product(Model model){
+		//using id=1 as a test
+		List<Product> product = product_repository.findByid(1);
+		model.addAttribute("product_id", product);
+		return "product";
+	}
 
 }
