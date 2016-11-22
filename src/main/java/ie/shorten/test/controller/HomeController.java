9e5baff5fa69751c5f81a3531c213d97f5006578
Dara Shorten <dara.shorten@gmail.com>
2016-11-22 12:53:42 +0000

    Created function to direct user to index page

--- src/main/java/ie/shorten/test/controller/HomeController.java
+++ src/main/java/ie/shorten/test/controller/HomeController.java
@@ -1,11 +1,13 @@
 package ie.shorten.test.controller;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ie.shorten.test.entity.Product;
 import ie.shorten.test.repository.ProductRepository;
 
 @Controller
@@ -14,8 +16,10 @@ public class HomeController {
 	ProductRepository product_repository;
 	
 	@RequestMapping("/")
-	public String home(Locale locale, Model model){
-		return "home";
+	public String index(Locale locale, Model model){
+		List<Product> product_list = product_repository.findAllproducts();
+		model.addAttribute(product_list);
+		return "index";
 	}
 	
 }
