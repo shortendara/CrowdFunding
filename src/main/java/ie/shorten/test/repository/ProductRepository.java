f807148c409d256176312e7901f6ee23a927b8b1
Dara Shorten <dara.shorten@gmail.com>
2016-11-22 13:07:10 +0000

    Added comments for functions

--- src/main/java/ie/shorten/test/repository/ProductRepository.java
+++ src/main/java/ie/shorten/test/repository/ProductRepository.java
@@ -7,10 +7,26 @@ import ie.shorten.test.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer>{
 
+	/**
+	 * Find products by product name
+	 * @Param productName
+	 * @return Product
+	 * */
 	public List<Product> findByproductName(String productName); // SELECT * FROM artists WHERE fullName LIKE '%xxxxx%'
 	public List<Product> findByproductNameContains(String productName); // SELECT * FROM artists WHERE fullName LIKE '%xxxxx%'
 	public List<Product> findByproductNameContainsIgnoreCase(String productName); // SELECT * FROM artists WHERE LOWER(fullName) LIKE LOWER('%xxxxx%')
+	
+	/**
+	 * Find all products that available
+	 * @return All Products
+	 */
 	public List<Product> findAll();
+	
+	/**
+	 * Find product by sepcific id number
+	 * @param id
+	 * @return Product
+	 */
 	public List<Product> findByid(int id);
 	
 }
