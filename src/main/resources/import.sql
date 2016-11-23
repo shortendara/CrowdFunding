ff6ff45d07d076173dc89044b2f60c7d9ea4ae7a
Dara Shorten <dara.shorten@gmail.com>
2016-11-23 11:51:28 +0000

    Added more dummy data to sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 INSERT INTO users VALUES (1,'Chadwick_Lynn'),(2,'Ono, Yoko'),(3,'Opie, Julian'),(4,'Etty, William'),(5,'Wallis, Henry');
 
 INSERT INTO products VALUES (1, 500.50,'Light Bulb',2000.0,'Emmits Light');
+INSERT INTO products VALUES	(2, 500.50,'Light Bulb',2000.0,'Emmits Light');
 							
 INSERT INTO users_products VALUES (1,1);
