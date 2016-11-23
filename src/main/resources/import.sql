d52c9e164e895e3412e89156816709da6784741b
Dara Shorten <dara.shorten@gmail.com>
2016-11-23 10:44:15 +0000

    Changes format of insert into products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO users VALUES (1,'Chadwick_Lynn'),(2,'Ono, Yoko'),(3,'Opie, Julian'),(4,'Etty, William'),(5,'Wallis, Henry');
 
-INSERT INTO products VALUES (1,'Light Bulb' , 'Emmits Light' , 2000.0, 500.50);
+INSERT INTO products VALUES (1, 500.50,'Light Bulb',2000.0,'Emmits Light');
 							
-INSERT INTO users_products VALUES (1,1),(1,2),(3,3),(4,4),(5,4);
+INSERT INTO users_products VALUES (1,1);
