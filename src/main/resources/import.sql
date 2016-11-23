bb96d6cb95c9e02184e069ce45cdf97cca803aa5
Dara Shorten <dara.shorten@gmail.com>
2016-11-23 15:03:56 +0000

    Added password fields to sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,6 @@
-CREATE TABLE USERS ( 
-  id int(11) NOT NULL AUTO_INCREMENT, 
-  USERNAME varchar(100) NOT NULL, 
-  PASSWORD varchar(10) NOT NULL, 
-  PRIMARY KEY (id) 
-);
 
-INSERT INTO users VALUES (1,'Chadwick_Lynn'),(2,'Ono, Yoko'),(3,'Opie, Julian'),(4,'Etty, William'),(5,'Wallis, Henry');
+
+INSERT INTO users VALUES (1,'Chadwick_Lynn', 123),(2,'Ono, Yoko', 'password'),(3,'Opie, Julian', 456),(4,'Etty, William', 'pass.word'),(5,'Wallis, Henry', 654);
 
 INSERT INTO products VALUES (1, 500.50,'Light Bulb',2000.0,'Emmits Light');
 INSERT INTO products VALUES	(2, 500.50,'Light Bulb',2000.0,'Emmits Light');
