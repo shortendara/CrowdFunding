3f030e04213bd5a0c2d99d49f4cd8b22389065d7
Dara Shorten <dara.shorten@gmail.com>
2016-12-06 20:17:12 +0000

    Added user ID to products table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,6 @@
 INSERT INTO users VALUES (1, 'password', 'dara_shorten'),(2, 'password', 'ogorman_liam'),(3, 'password', 'darren_sisk'),(4, 'password', 'kenneth');
 
 /*Product ID, current_raised, end_date, product_description, product_goal, product_name, start_date, youtube_url*/
-INSERT INTO products VALUES (1, 500.50, TO_DATE('31/01/2017', 'DD/MM/YYYY'),'Emmits Light',2000.0,'Light Bulb', TO_DATE('01/12/2016', 'DD/MM/YYYY'), 'http://youtube.com/embed/ClHu3XQBnGU');
-INSERT INTO products VALUES (2, 300, TO_DATE('15/01/2017', 'DD/MM/YYYY'), 'Form of transportation', 10000.0,'Car', TO_DATE('12/09/2016', 'DD/MM/YYYY'), 'http://youtube.com/embed/ClHu3XQBnGU');
-							
-INSERT INTO users_products VALUES (1,1);
+INSERT INTO products VALUES (1, 500.50, TO_DATE('31/01/2017', 'DD/MM/YYYY'),'Emmits Light',2000.0,'Light Bulb', TO_DATE('01/12/2016', 'DD/MM/YYYY'), 'http://youtube.com/embed/ClHu3XQBnGU', 1);
+INSERT INTO products VALUES (2, 300, TO_DATE('15/01/2017', 'DD/MM/YYYY'), 'Form of transportation', 10000.0,'Car', TO_DATE('12/09/2016', 'DD/MM/YYYY'), 'http://youtube.com/embed/ClHu3XQBnGU', 2);
+
