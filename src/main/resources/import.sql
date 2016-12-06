a9e338098d9622efb528630032bf331838109ff6
Liam O Gorman <L.ogorman@mycit.ie>
2016-12-06 16:30:34 +0000

    Added log in authentication for a single user

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 
 
-INSERT INTO users VALUES (1,'dara_shorten', 123),(2,'ogorman_liam', 'password'),(3,'darren_sisk', 456),(4,'king_kenny', 'pass.word');
+INSERT INTO users VALUES (1, 'password', 'dara_shorten'),(2, 'password', 'ogorman_liam'),(3, 'password', 'darren_sisk'),(4, 'password', 'kenneth');
 
 /*Product ID, current_raised, end_date, product_description, product_goal, product_name, start_date, youtube_url*/
 INSERT INTO products VALUES (1, 500.50, TO_DATE('31/01/2017', 'DD/MM/YYYY'),'Emmits Light',2000.0,'Light Bulb', TO_DATE('01/12/2016', 'DD/MM/YYYY'), 'http://youtube.com/embed/ClHu3XQBnGU');
