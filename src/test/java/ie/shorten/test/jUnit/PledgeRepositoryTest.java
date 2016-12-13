879721552e0d33a3c38b065b7c48571e17f4068c
Liam O Gorman <L.ogorman@mycit.ie>
2016-12-13 16:57:31 +0000

    Made changes to jUnit classes, fixed merge comments

--- src/test/java/ie/shorten/test/jUnit/PledgeRepositoryTest.java
+++ src/test/java/ie/shorten/test/jUnit/PledgeRepositoryTest.java
@@ -6,19 +6,28 @@ package ie.shorten.test.jUnit;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.stereotype.Repository;
 
 import ie.shorten.test.entity.Pledge;
+import ie.shorten.test.entity.Product;
+import ie.shorten.test.entity.User;
 import ie.shorten.test.repository.PledgeRepository;
+import ie.shorten.test.repository.ProductRepository;
+import ie.shorten.test.repository.UserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PledgeRepositoryTest {
- 
+
+	UserRepository userRepo;
+	
     @Mock
     private PledgeRepository pledgeRepo;
     
@@ -26,7 +35,14 @@ public class PledgeRepositoryTest {
     public void findByPledgeId() {
  
         // Expected objects
-        List<Pledge> expectedPledge = pledgeRepo.findByuser_id(1);
+    	User u = (User) userRepo.findByid(1);
+    	Date start = new Date();
+    	Date end = new Date();
+    	Product product = new Product(1, "productName", "productDescription", 1000, 100, start, end, "youtube_url");
+    	Pledge p = new Pledge(1,1000.0, u, product);
+        List<Pledge> expectedPledge = new ArrayList<Pledge>(); 
+        expectedPledge.add(p);
+        
  
         // Mockito expectations                            
         when(pledgeRepo.findAll()).thenReturn(expectedPledge);
